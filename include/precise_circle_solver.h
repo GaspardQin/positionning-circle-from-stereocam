e2817f0fa707d4ced189ba2116dbe8f0c6ce5500
Chuan QIN <gaspard.qin@gmail.com>
2019-04-29 00:31:48 +0800

    add costfactor for ceres

--- include/precise_circle_solver.h
+++ include/precise_circle_solver.h
@@ -3,8 +3,8 @@
 #include "base_circle_solver.h"
 #include "circle_model.h"
 #include "camera_model.h"
-#include <unsupported/Eigen/NumericalDiff>
-#include <unsupported/Eigen/NonLinearOptimization>
+#include "ceres/ceres.h"
+#include "glog/logging.h"
 
 template <class CircleT>
 class PreciseCircleSolverBaseImpl:public BaseCircleSolver{
@@ -51,6 +51,7 @@ public:
 
     virtual void getReprojectPoints(const Eigen::VectorXd &param, std::vector<float>& left_reproject_points, 
                                     std::vector<float>& right_reproject_points, const int n_point)=0;
+    virtual void getReprojectPoint(const EigenVector8d& param, const Eigen::Vector3d& point_in_plane,  Eigen::Vector3d& left_project_point,  Eigen::Vector3d& right_project_point)=0;
     template<int NX = Eigen::Dynamic,int NY = Eigen::Dynamic>
     struct LMFunctor //used by LM algorithm proposed by Eigen
     {	
@@ -116,7 +117,7 @@ public:
         
         int df(const Eigen::VectorXd &x, Eigen::MatrixXd &fjac) const
         {
-            fjac.resize(1, m);
+            fjac.resize(1, n);
             Eigen::VectorXd direction;
             direction.resize(n,1);
             Eigen::VectorXd fvec;
@@ -149,6 +150,38 @@ public:
 
 };
 
+struct EdgeMinimalDistance {
+    EdgeMinimalDistance(double x, double y, PreciseCircleSolverBaseImpl* solver_ptr_input):solver_ptr(solver_ptr_input){
+        point_in_plane<<x, y, 1.0;
+    };
+    bool operator()(const double* const x, double* residual) const {
+        residual[0] = 10.0 - x[0];
+        Eigen::Vector3d left_project_point,  Eigen::Vector3d right_project_point;
+        EigenVector8d param;
+        param << x[0], x[1], x[2],x[3],x[4],x[5],x[6],x[7];
+        solver_ptr->getReprojectPoint(param, point_in_plane, left_project_point, right_project_point);
+        std::vector<int> indices_left, indices_right;
+        std::vector<float> dists_left, dists_right;
+
+        std::vector<float> flattern_left_project_point(2);
+        std::vector<float> flattern_right_project_point(2);
+
+        flattern_left_project_point.push_back(left_project_point(0));
+        flattern_left_project_point.push_back(left_project_point(1));
+        flattern_right_project_point.push_back(right_project_point(0));
+        flattern_right_project_point.push_back(right_project_point(0));
+
+        solver_ptr->kdtree_left.knnSearch(flattern_left_project_point, indices_left, dists_left, 1, cv::flann::SearchParams());
+        solver_ptr->kdtree_right.knnSearch(flattern_right_project_point, indices_right, dists_right, 1, cv::flann::SearchParams());
+        
+        residual[0] = dists_left[0] + dists_right[0];
+        return true;
+    }
+private:
+    Eigen::Vector3d point_in_plane;
+    PreciseCircleSolverBaseImpl* solver_ptr;
+};
+
 
 class PreciseTwoConcentricCirclesSolver: public PreciseCircleSolverBaseImpl<ConcentricCircles3D>{
 private:
@@ -159,6 +192,24 @@ private:
     }
 public:
     PreciseTwoConcentricCirclesSolver(std::shared_ptr<StereoCameraModel>& stereo_cam_ptr_):PreciseCircleSolverBaseImpl(stereo_cam_ptr_){};
+
+    virtual void getReprojectPoint(const EigenVector8d& param, const Eigen::Vector3d& point_in_plane,  Eigen::Vector3d& left_project_point,  Eigen::Vector3d& right_project_point){
+
+        Eigen::Matrix4d transform;
+        Eigen::Matrix3d rot = Eigen::AngleAxisd(param(3), Eigen::Vector3d::UnitX()).toRotationMatrix()
+          * Eigen::AngleAxisd(param(4), Eigen::Vector3d::UnitY()).toRotationMatrix()
+          * Eigen::AngleAxisd(param(5), Eigen::Vector3d::UnitZ()).toRotationMatrix();
+
+        transform.block<3,3>(0,0) = rot;
+        transform.block<3,1>(0,3) = param.segment(0,3);
+        double radius_inner = param(6);
+        double radius_outer = param(7);
+
+        left_project_point = stereo_cam_ptr->left.projection_mat * transform * point_in_plane;
+        right_project_point = stereo_cam_ptr->right.projection_mat * transform * point_in_plane;
+        normalize(left_projection_point);
+        normalize(right_projection_point);
+    }
 
     virtual void getReprojectPoints(const Eigen::VectorXd &param, std::vector<float>& left_reproject_points, 
                                     std::vector<float>& right_reproject_points, const int n_point){
